75fa3bdea4dcbd7af4beca9d061eca343ddaa209
carol1naa07 <carolina.lemos@escolar.ifrn.edu.br>
2025-10-21 08:10:40 -0300

    subir

--- todo-list-mvc/app/models/tarefas.php
+++ todo-list-mvc/app/models/tarefas.php
@@ -9,7 +9,7 @@ class Tarefa {
         $this->conn = $db->conectar ();
     }
 
-    ## 
+    ##Listar 
    public function listar (){
     $tarefas = [];
     $sql = "SELECT * FROM tarefas ORDER BY data_criacao DESC";
@@ -18,9 +18,21 @@ class Tarefa {
     if( $resultado->num_rows > 0) {
          while( $row = $resultado->fetch_assoc () ) 
          $tarefas[] = $row;
+        
    }
-
+      return $tarefas;
+  }
+  ##Criar
+  public function criar ($descricao) {
+    $descricao = $this->conn->real_escape_string($descricao);
+    $sql = "INSERT INTO  tarefas (descricao) VALUES ('$descricao')";
+    return $this->conn->query ( $sql );
 }
-
+  ##Excluir 
+  public function excluir ($id){
+   $id = intval($id);
+   $sql = "DELETE FROM tarefas WHERE id = $id";
+   return $this->conn->query ( $sql );
+  }
 }
 ?>
